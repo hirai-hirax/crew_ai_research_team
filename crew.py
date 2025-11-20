f63fcef4ace301c013759c6670fc24c37a867647
hirai-hirax <hir314@gmail.com>
2025-11-21 08:22:48 +0900

    Refactor weekend planner into multi-agent flow

--- crew.py
+++ crew.py
@@ -57,16 +57,84 @@ class WeekendPlanner:
     tasks_config = 'config/tasks.yaml'
 
     @agent
-    def weekend_planner(self) -> Agent:
+    def weather_specialist(self) -> Agent:
         return Agent(
-            config=self.agents_config['weekend_planner'],
+            config=self.agents_config['weather_specialist'],
             verbose=True,
             tools=[SerperDevTool()],
         )
 
     @task
-    def recommend_weekend(self) -> Task:
-        return Task(config=self.tasks_config['weekend_recommendation'])
+    def fetch_weather(self) -> Task:
+        return Task(config=self.tasks_config['fetch_weather'])
+
+    @agent
+    def calendar_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config['calendar_analyst'],
+            verbose=True,
+        )
+
+    @task
+    def analyze_calendar(self) -> Task:
+        return Task(config=self.tasks_config['analyze_calendar'])
+
+    @agent
+    def local_scout(self) -> Agent:
+        return Agent(
+            config=self.agents_config['local_scout'],
+            verbose=True,
+            tools=[SerperDevTool()],
+        )
+
+    @task
+    def explore_local_options(self) -> Task:
+        return Task(config=self.tasks_config['explore_local_options'])
+
+    @agent
+    def recommendation_curator(self) -> Agent:
+        return Agent(
+            config=self.agents_config['recommendation_curator'],
+            verbose=True,
+        )
+
+    @task
+    def craft_recommendations(self) -> Task:
+        return Task(config=self.tasks_config['craft_recommendations'])
+
+    @agent
+    def transport_planner(self) -> Agent:
+        return Agent(
+            config=self.agents_config['transport_planner'],
+            verbose=True,
+            tools=[SerperDevTool()],
+        )
+
+    @task
+    def plan_transport(self) -> Task:
+        return Task(config=self.tasks_config['plan_transport'])
+
+    @agent
+    def calendar_scheduler(self) -> Agent:
+        return Agent(
+            config=self.agents_config['calendar_scheduler'],
+            verbose=True,
+        )
+
+    @task
+    def schedule_calendar_entry(self) -> Task:
+        return Task(config=self.tasks_config['schedule_calendar_entry'])
+
+    @agent
+    def itinerary_designer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['itinerary_designer'],
+            verbose=True,
+        )
+
+    @task
+    def build_itinerary(self) -> Task:
+        return Task(config=self.tasks_config['build_itinerary'])
 
     @crew
     def crew(self) -> Crew:
@@ -74,7 +142,15 @@ class WeekendPlanner:
 
         return Crew(
             agents=self.agents,
-            tasks=self.tasks,
+            tasks=[
+                self.fetch_weather(),
+                self.analyze_calendar(),
+                self.explore_local_options(),
+                self.craft_recommendations(),
+                self.plan_transport(),
+                self.schedule_calendar_entry(),
+                self.build_itinerary(),
+            ],
             process=Process.sequential,
             verbose=True,
         )
